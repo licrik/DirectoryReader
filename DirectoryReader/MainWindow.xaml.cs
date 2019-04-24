b189924019f17de0091da9ef22e5e98182ea6054
licrik <garghma.licrik@gmail.com>
2019-04-24 14:12:27 +0300

    first pull

---
+++ DirectoryReader/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DirectoryReader
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private object dummyNode = null;
+        private string text;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string SelectedImagePath { get; set; }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = s;
+                item.Tag = s;
+                item.FontWeight = FontWeights.Normal;
+                item.Items.Add(dummyNode);
+                item.Expanded += new RoutedEventHandler(folder_Expanded);
+                DirectoryTree.Items.Add(item);
+            }
+        }
+
+        void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            if (item.Items.Count == 1 && item.Items[0] == dummyNode)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        TreeViewItem subitem = new TreeViewItem();
+                        subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                        subitem.Tag = s;
+                        subitem.FontWeight = FontWeights.Normal;
+                        subitem.Items.Add(dummyNode);
+                        subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+                        item.Items.Add(subitem);
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void foldersItem_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            TreeView tree = (TreeView)sender;
+            TreeViewItem temp = ((TreeViewItem)tree.SelectedItem);
+            FlowDocument flowDocument = new FlowDocument();
+            text = null;
+
+            if (temp == null)
+                return;
+            SelectedImagePath = "";
+            string temp1 = "";
+            string temp2 = "";
+            while (true)
+            {
+                temp1 = temp.Header.ToString();
+                if (temp1.Contains(@"\"))
+                {
+                    temp2 = "";
+                }
+                SelectedImagePath = temp1 + temp2 + SelectedImagePath;
+                if (temp.Parent.GetType().Equals(typeof(TreeView)))
+                {
+                    break;
+                }
+                temp = ((TreeViewItem)temp.Parent);
+                temp2 = @"\";
+            }
+            //show user selected path
+            //MessageBox.Show(SelectedImagePath);
+            text += "Какталог: " + SelectedImagePath + "\n";
+            GetAllFiles(SelectedImagePath);
+
+            flowDocument.Blocks.Add(new Paragraph(new Run(text)));
+            InformationText.Document = flowDocument;
+            
+        }
+
+        private void GetAllFiles(string path)
+        {
+            foreach(string s in Directory.GetFiles(path.ToString()))
+            {
+                FileInfo fileINf = new FileInfo(s);
+
+                text += "\n Название файла: \n" + s + "\n Размер: " + fileINf.Length.ToString() + " байт;";  
+            }
+        }
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(SelectedImagePath + "Files.txt", text);
+        }
+    }
+}
